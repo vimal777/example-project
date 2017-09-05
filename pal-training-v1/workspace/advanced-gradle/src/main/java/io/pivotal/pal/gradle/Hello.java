75cdf9ded82ff976b764396413e62eed30fdd0ee
vimal777 <vimal.sawant@capgemini.com>
2017-09-06 01:49:14 +0530

    files commited

---
+++ pal-training-v1/workspace/advanced-gradle/src/main/java/io/pivotal/pal/gradle/Hello.java
@@ -0,0 +1,10 @@
+package io.pivotal.pal.gradle;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Messenger messenger = new Messenger();
+
+        System.out.println(messenger.getMessage());
+    }
+}
