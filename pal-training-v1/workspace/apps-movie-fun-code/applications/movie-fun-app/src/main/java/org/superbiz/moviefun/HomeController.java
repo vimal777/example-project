a26ef92e1004953140b1de4b5f7a01af837870da
vimal777 <vimal.sawant@capgemini.com>
2017-09-20 13:17:06 +0530

    microservice

--- pal-training-v1/workspace/apps-movie-fun-code/applications/movie-fun-app/src/main/java/org/superbiz/moviefun/HomeController.java
+++ pal-training-v1/workspace/apps-movie-fun-code/applications/movie-fun-app/src/main/java/org/superbiz/moviefun/HomeController.java
@@ -2,24 +2,24 @@ package org.superbiz.moviefun;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.superbiz.moviefun.albums.Album;
-import org.superbiz.moviefun.albums.AlbumFixtures;
-import org.superbiz.moviefun.albums.AlbumsBean;
-import org.superbiz.moviefun.movies.Movie;
-import org.superbiz.moviefun.movies.MovieFixtures;
-import org.superbiz.moviefun.movies.MoviesBean;
+import org.superbiz.moviefun.albumsapi.AlbumFixtures;
+import org.superbiz.moviefun.albumsapi.AlbumInfo;
+import org.superbiz.moviefun.albumsapi.AlbumsClient;
+import org.superbiz.moviefun.moviesapi.MovieFixtures;
+import org.superbiz.moviefun.moviesapi.MovieInfo;
+import org.superbiz.moviefun.moviesapi.MoviesClient;
 
 import java.util.Map;
 
 @Controller
 public class HomeController {
 
-    private final MoviesBean moviesBean;
-    private final AlbumsBean albumsBean;
+    private final MoviesClient moviesBean;
+    private final AlbumsClient albumsBean;
     private final MovieFixtures movieFixtures;
     private final AlbumFixtures albumFixtures;
 
-    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+    public HomeController(MoviesClient moviesBean, AlbumsClient albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
         this.moviesBean = moviesBean;
         this.albumsBean = albumsBean;
         this.movieFixtures = movieFixtures;
@@ -33,11 +33,11 @@ public class HomeController {
 
     @GetMapping("/setup")
     public String setup(Map<String, Object> model) {
-        for (Movie movie : movieFixtures.load()) {
+        for (MovieInfo movie : movieFixtures.load()) {
             moviesBean.addMovie(movie);
         }
 
-        for (Album album : albumFixtures.load()) {
+        for (AlbumInfo album : albumFixtures.load()) {
             albumsBean.addAlbum(album);
         }
 
